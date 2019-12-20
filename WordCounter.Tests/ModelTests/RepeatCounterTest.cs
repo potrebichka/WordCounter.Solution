74117ff441295766172099616cf7a6b6ef18feaf
Nina Potrebich <potrebich@gmail.com>
2019-12-20 10:59:38 -0800

    add front end logic

--- WordCounter.Tests/ModelTests/RepeatCounterTest.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTest.cs
@@ -21,7 +21,7 @@ namespace WordCounter.Tests
             Assert.AreEqual(0, result);
         }
         [TestMethod]
-        public void RepeatCounter_WordEncounterInSentence_Count()
+        public void RepeatCounter_WordEncounterInSentenceSingleWords_Count()
         {
             RepeatCounter testInstance = new RepeatCounter("A cat sentence with a cat.", "cat");
             int result = testInstance.Count();
@@ -40,6 +40,13 @@ namespace WordCounter.Tests
             RepeatCounter testInstance = new RepeatCounter("A sentence with a cathedral", "cat");
             int result = testInstance.Count();
             Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void RepeatCounter_WordEncounterInSentence_Count()
+        {
+            RepeatCounter testInstance = new RepeatCounter("A cat sentence with a cathedral", "cat");
+            int result = testInstance.Count();
+            Assert.AreEqual(1, result);
         }
     }
 }
--- WordCounter/WordCounter.cs
+++ WordCounter/WordCounter.cs
@@ -7,6 +7,37 @@ namespace WordCounter
     {
         public static void Main()
         {
+            Console.WriteLine("Welcome to Word Counter Program");
+            while (true)
+            {
+                Console.WriteLine("Enter a sentence:");
+                string sentence = Console.ReadLine();
+                Console.WriteLine("Enter a word:");
+                string word = Console.ReadLine();
+                RepeatCounter counter = new RepeatCounter(sentence, word);
+                int count = counter.Count();
+                Console.WriteLine($"Word '{word}' appears in sentence '{sentence}'  {count} times");
+                bool flag = false;
+                while (true)
+                {
+                    Console.WriteLine("Do you want to repeat?[yes,no]");
+                    string userInput = Console.ReadLine();
+                    if (userInput == "yes")
+                    {
+                        break;
+                    }
+                    else if (userInput == "no")
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag) 
+                {
+                    break;
+                }
+
+            }
 
         }
     }
