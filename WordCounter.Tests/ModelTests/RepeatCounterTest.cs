1aac1152b5560b44928d531d1053b4bf23437ce8
Nina Potrebich <potrebich@gmail.com>
2019-12-20 10:07:43 -0800

    add test for counting how many times word in sentence

--- WordCounter.Tests/ModelTests/RepeatCounterTest.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTest.cs
@@ -14,11 +14,18 @@ namespace WordCounter.Tests
             Assert.AreEqual(typeof(RepeatCounter), testInstance.GetType());
         }
         [TestMethod]
-        public void RepeatCounter_WordNotAppearInSentence_Zero()
+        public void RepeatCounter_WordNotEncounterInSentence_Zero()
         {
-            RepeatCounter testInstance = new RepeatCounter("Just a plain sentence", "cat");
+            RepeatCounter testInstance = new RepeatCounter("Just a plain sentence.", "cat");
             int result = testInstance.Count();
             Assert.AreEqual(result, 0);
+        }
+        [TestMethod]
+        public void RepeatCounter_WordEncounterInSentence_Count()
+        {
+            RepeatCounter testInstance = new RepeatCounter("A cat sentence with a cat.", "cat");
+            int result = testInstance.Count();
+            Assert.AreEqual(result, 2);
         }
     }
 }
