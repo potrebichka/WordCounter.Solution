cee71feb26515830e9380aa05dece613a318f8b9
Nina Potrebich <potrebich@gmail.com>
2019-12-20 11:51:59 -0800

    add styling

--- WordCounter/WordCounter.cs
+++ WordCounter/WordCounter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using WordCounter.Models;
 
 namespace WordCounter
@@ -7,23 +8,36 @@ namespace WordCounter
     {
         public static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
             Console.WriteLine("Welcome to Word Counter Program");
+            Console.ResetColor();
             while (true)
             {
                 Console.WriteLine("Enter a sentence:");
+                Console.ForegroundColor = ConsoleColor.Blue;
                 string sentence = Console.ReadLine();
+                Console.ResetColor();
                 Console.WriteLine("Enter a word:");
+                Console.ForegroundColor = ConsoleColor.Blue;
                 string word = Console.ReadLine();
+                Console.ResetColor();
                 RepeatCounter counter = new RepeatCounter(sentence, word);
                 int count = counter.Count();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"Word '{word}' appears in sentence '{sentence}'  {count} times");
+                Console.ResetColor();
+                Console.WriteLine("------------------------");
                 bool flag = false;
                 while (true)
                 {
                     Console.WriteLine("Do you want to repeat?[yes,no]");
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     string userInput = Console.ReadLine();
+                    Console.ResetColor();
                     if (userInput == "yes")
                     {
+                        Console.WriteLine("------------------------");
                         break;
                     }
                     else if (userInput == "no")
@@ -38,7 +52,7 @@ namespace WordCounter
                 }
 
             }
-
+            Console.ResetColor();
         }
     }
 }
